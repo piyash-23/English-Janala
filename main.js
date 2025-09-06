3960d4584ff4eba380a4f9e3e62e20f7e6d2af37
piyash-23 <arafat.ahmed2004@gmail.com>
2025-09-06 23:04:33 +0600

    dynamic words

--- main.js
+++ main.js
@@ -9,17 +9,62 @@ const loadLevel = () => {
 loadLevel();
 
 const showWords = (words) => {
-  console.log(words);
+  // console.log(words);
+  let primary = document.querySelector(".primary");
+  primary.innerHTML = "";
+
+  if (words.length == 0) {
+    primary.innerHTML = `
+    <img  src="./assets/alert-error.png" alt="logo" />
+    <p id="select" class="bangla text-sm">
+                এই <span class="english">Lesson</span> এ এখনো কোন <span class="english">Vocabulary</span> যুক্ত করা হয়নি।
+              </p>
+              <h3 id="lesson" class="bangla">
+                নেক্সট <span class="english">Lesson</span> এ যান
+              </h3>`;
+    return;
+  }
+
   words.forEach((word) => {
-    console.log(word);
+    // console.log(word);
+
+    let card = document.createElement("div");
+    primary.classList.add("grid");
+    card.innerHTML = `
+    <div class="card">
+              <h2 class="word enlish">${word.word}</h2>
+              <p class="subtitle enlish">Meaning/Pronounciation</p>
+              <h1 class="meaning bangla">"${
+                word.meaning === null ? "অর্থ পাওয়া যায়নি" : word.meaning
+              }/${word.pronunciation}"</h1>
+              <div class="info">
+                <button class="detail" id="information">
+                  <i class="fa-solid fa-circle-info"></i>
+                </button>
+                <button class="detail" id="sound">
+                  <i class="fa-solid fa-volume-high"></i>
+                </button>
+              </div>
+            </div> 
+    `;
+
+    primary.appendChild(card);
   });
 };
-
+const removeActive = () => {
+  let loadbtn = document.querySelectorAll(".load-btn"); //remove class
+  for (let btn of loadbtn) {
+    btn.classList.remove("active");
+  }
+};
 const loadWords = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((json) => {
+      let lessonBtn = document.getElementById(`lesson-btn-${id}`);
+      removeActive();
+      lessonBtn.classList.add("active");
       showWords(json.data);
     });
 };
@@ -29,7 +74,7 @@ const levelShow = (levels) => {
   levels.forEach((level) => {
     let btn = document.createElement("div");
     btn.innerHTML = `
-    <button class="pri-btn" onclick='loadWords(${level.level_no})'>
+    <button class="pri-btn load-btn" id="lesson-btn-${level.level_no}" onclick='loadWords(${level.level_no})'>
             <i class="fa-solid fa-book-open"></i> Lesson - ${level.level_no}
           </button>`;
     btnBox.append(btn);
