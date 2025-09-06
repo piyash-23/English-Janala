c7268972ad0c3bb832da1d57766eb04513058631
piyash-23 <arafat.ahmed2004@gmail.com>
2025-09-06 13:54:55 +0600

    words

--- main.js
+++ main.js
@@ -8,13 +8,28 @@ const loadLevel = () => {
 
 loadLevel();
 
+const showWords = (words) => {
+  console.log(words);
+  words.forEach((word) => {
+    console.log(word);
+  });
+};
+
+const loadWords = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      showWords(json.data);
+    });
+};
+
 const levelShow = (levels) => {
-  console.log(levels);
   let btnBox = document.getElementById("btnBox");
   levels.forEach((level) => {
     let btn = document.createElement("div");
     btn.innerHTML = `
-    <button class="pri-btn">
+    <button class="pri-btn" onclick='loadWords(${level.level_no})'>
             <i class="fa-solid fa-book-open"></i> Lesson - ${level.level_no}
           </button>`;
     btnBox.append(btn);
